e52bda71f84c8dda2713cf3098e9ce5efcd4c35f
Stanislav <stanislav@labushniak.ru>
2021-01-24 22:44:33 +0300

    add views with raw html

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('users');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/create', function () {
+    return view('create');
+});
+
+Route::get('/edit', function () {
+    return view('edit');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/security', function () {
+    return view('security');
+});
+
+Route::get('/status', function () {
+    return view('status');
+});
+
+Route::get('/avatar', function () {
+    return view('avatar');
 });
