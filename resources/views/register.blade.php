e0171cb5a74206dea0700b5ad0c5176b5a38cc58
Stanislav <stanislav@labushniak.ru>
2021-02-04 23:23:13 +0300

    minor update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,7 +9,6 @@
     <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
     <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
     <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
     <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#5bbad5">
     <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
 @endsection
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -168,12 +168,12 @@
         <!-- BEGIN Page Footer -->
         <footer class="page-footer" role="contentinfo">
             <div class="d-flex align-items-center flex-1 text-muted">
-                <span class="hidden-md-down fw-700">2020 © Учебный проект</span>
+                <span class="hidden-md-down fw-700">2021 © Учебный проект</span>
             </div>
             <div>
                 <ul class="list-table m-0">
-                    <li><a href="intel_introduction.html" class="text-secondary fw-700">Home</a></li>
-                    <li class="pl-3"><a href="info_app_licensing.html" class="text-secondary fw-700">About</a></li>
+                    <li><a href="{{ route('home') }}" class="text-secondary fw-700">Home</a></li>
+                    
                 </ul>
             </div>
         </footer>
